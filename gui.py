120fd2f3185f8faa8b02c47efc448f2e87102aeb
Raj Kumar Chaudhary <rajkumarchaudhary26@gmail.com>
2022-08-29 15:15:36 +0545

    Minor fix

--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
-import tkinter
-window = tkinter.Tk('Training')
-import os
-from  dataset import index
-from database import addStudent,database,fetch_data
+from database import addStudent, database, fetch_data
 from traning import training
+from dataset import index
+import os
+import tkinter
+window = tkinter.Tk()
 
 
 def submit():
@@ -12,35 +12,39 @@ def submit():
     emailAddress = email.get()
     address = cAddress.get()
     # try:
-    db =  database()
-    addStudent(db,fName,lName,emailAddress,address)
-    id,name = fetch_data()
-    path = 'C:/Users/Raj/Desktop/dataset/{}/'.format(id)
+    db = database()
+    addStudent(db, fName, lName, emailAddress, address)
+    id, name = fetch_data()
+    # print(id)
+    path = 'dataset/{}/'.format(id)
+    # if not os.path.exists(path):
+    #     os.makedirs(path)
     os.mkdir(path)
     index(path)
-    
+
     # except:
     #     print("Error")
-    
 
-lblFirstName = tkinter.Label(window,text="First Name").place(x=0,y=0)
+
+lblFirstName = tkinter.Label(window, text="First Name").place(x=0, y=0)
 fn = tkinter.StringVar()
-entryFirstName = tkinter.Entry(window,textvar=fn).place(x=70,y=0)
+entryFirstName = tkinter.Entry(window, textvar=fn).place(x=70, y=0)
 
-lblLastName = tkinter.Label(window,text="Last Name").place(x=0,y=40)
+lblLastName = tkinter.Label(window, text="Last Name").place(x=0, y=40)
 ln = tkinter.StringVar()
-entryLastName = tkinter.Entry(window,textvar=ln).place(x=70,y=40)
+entryLastName = tkinter.Entry(window, textvar=ln).place(x=70, y=40)
 
-lblEmail = tkinter.Label(window,text="Email").place(x=0,y=80)
+lblEmail = tkinter.Label(window, text="Email").place(x=0, y=80)
 email = tkinter.StringVar()
-entryEmail = tkinter.Entry(window,textvar=email).place(x=70,y=80)
+entryEmail = tkinter.Entry(window, textvar=email).place(x=70, y=80)
 cAddress = tkinter.StringVar()
-lblAddress = tkinter.Label(window,text="Address").place(x=0,y=120)
-entryAddress = tkinter.Entry(window,textvar=cAddress).place(x=70,y=120)
+lblAddress = tkinter.Label(window, text="Address").place(x=0, y=120)
+entryAddress = tkinter.Entry(window, textvar=cAddress).place(x=70, y=120)
 
 
-button = tkinter.Button(window,text='Submit',command=submit).place(x=170,y=190)
-button = tkinter.Button(window,text='Train',command=training).place(x=230,y=190)
+button = tkinter.Button(window, text='Submit',
+                        command=submit).place(x=170, y=190)
+button = tkinter.Button(window, text='Train',
+                        command=training).place(x=230, y=190)
 window.geometry('500x500')
 window.mainloop()
-
