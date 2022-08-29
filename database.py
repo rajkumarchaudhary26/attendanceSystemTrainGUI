345bbeedcc9d25bbd1e10d89c5260af4040536ce
Raj Kumar Chaudhary <rajkumarchaudhary26@gmail.com>
2022-08-29 15:13:29 +0545

    Fix database connector

--- database.py
+++ database.py
@@ -1,35 +1,42 @@
-import MySQLdb
+import mysql.connector
 import datetime
+
+
 def database():
-    db = MySQLdb.connect("localhost","root","","attendance" )
+    db = mysql.connector.connect(
+        host="localhost", user="root", password="", db="attendance")
     return db
 
-def addStudent(db,firstName,LAstName,Email,Address):
-    now = datetime.datetime.now(); 
+
+def addStudent(db, firstName, LAstName, Email, Address):
+    now = datetime.datetime.now()
     currentDate = now.strftime("%Y-%m-%d")
     cursor = db.cursor()
-    sql = "INSERT INTO attendanceapp_student(first_name,last_name, email, address,created_date,updated_date) VALUES ('{}','{}','{}','{}','{}','{}')".format(firstName,LAstName,Email,Address,currentDate,currentDate)
+    sql = "INSERT INTO AttendanceApp_student(first_name,last_name, email, address,created_date,updated_date) VALUES ('{}','{}','{}','{}','{}','{}')".format(
+        firstName, LAstName, Email, Address, currentDate, currentDate)
     cursor.execute(sql)
     db.commit()
+    # db.close()
 
 
 def fetch_data():
-    db = MySQLdb.connect("localhost","root","","attendance")
+    db = mysql.connector.connect(
+        host="localhost", user="root", password="", db="attendance")
 
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
 
-    sql = "SELECT id,first_name FROM attendanceapp_student"
+    sql = "select id,first_name from AttendanceApp_student"
     try:
         # Execute the SQL command
         cursor.execute(sql)
-            # Fetch all the rows in a list of lists.
+        # Fetch all the rows in a list of lists.
         results = cursor.fetchall()[-1]
         id = results[0]
         name = results[1]
-        return id,name
+        return id, name
     except:
-        print ("Error: unable to fecth data")
+        print("Error: unable to fetch data")
 
         # disconnect from server
     db.close()
